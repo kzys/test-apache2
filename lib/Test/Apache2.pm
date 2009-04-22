ace0865f40a2e709ae6f78b0bde35266cc37fc0a
Kato Kazuyoshi <kato.kazuyoshi@gmail.com>
2009-04-23 02:19:38 +0900

    Added POD.

--- lib/Test/Apache2.pm
+++ lib/Test/Apache2.pm
@@ -22,3 +22,29 @@ sub import {
 }
 
 1;
+__END__
+
+=head1 NAME
+
+Test::Apache2 - Testing mod_perl handler without httpd (1)
+
+=head1 SYNOPSIS
+
+  use Test::Apache2;
+
+=head1 DESCRIPTION
+
+Test::Apache2 is a test harness of mod_perl handler.
+
+=head1 AUTHOR
+
+KATO Kazuyoshi E<lt>kzys@8-p.infoE<gt>
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 SEE ALSO
+
+L<Apache::Test>, L<Test::Environment>, L<Apache2::ASP>
+
+=cut
